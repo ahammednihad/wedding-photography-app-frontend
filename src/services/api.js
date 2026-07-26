e252e9b04916fdb8a6857b299d57821b470d0185
Ahammed Nihad <ahammednihad02@gmail.com>
2026-07-27 00:27:40 +0530

    api update

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 import { API_BASE_URL, STORAGE_KEYS } from "../utils/constants";
 
+
 // Create axios instance with default config
 const apiClient = axios.create({
-  baseURL: API_BASE_URL, // http://localhost:5000/api
+  baseURL: import.meta.env.VITE_API_BASE_URL, // http://localhost:5000/api
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
